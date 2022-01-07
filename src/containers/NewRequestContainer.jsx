5c638911a8a17a98bd6088fe797d74f712209c2c
budryte <43143936+budryte@users.noreply.github.com>
2022-01-07 18:52:16 +0200

    added information passing between tabs

--- src/containers/NewRequestContainer.jsx
+++ src/containers/NewRequestContainer.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import { RequestDetails } from "../components/request-form/RequestDetails";
 import { AttachFile } from "../components/request-form/AttachFile";
+import { ReviewAndSend } from "../components/request-form/ReviewAndSend";
 
 import "./new-request.scss";
 
@@ -30,8 +31,10 @@ function TabPanel(props) {
 
 export function NewRequestContainer() {
   const [value, setValue] = useState(0);
+  const [details, setDetails] = useState(null);
+  const [extraInfo, setExtraInfo] = useState("");
 
-  const handleChange = (event, newValue) => {
+  const handleChange = (_, newValue) => {
     setValue(newValue);
   };
 
@@ -46,15 +49,29 @@ export function NewRequestContainer() {
         >
           <Tab label="Details" />
           <Tab label="Attach Files" />
-          <Tab label="Review & Send" disabled />
+          <Tab label="Review & Send" />
         </Tabs>
         <TabPanel value={value} index={0}>
-          <RequestDetails />
+          <RequestDetails
+            handleChange={handleChange}
+            details={details}
+            setDetails={setDetails}
+          />
         </TabPanel>
         <TabPanel value={value} index={1}>
-          <AttachFile />
+          <AttachFile
+            handleChange={handleChange}
+            extraInfo={extraInfo}
+            setExtraInfo={setExtraInfo}
+          />
         </TabPanel>
-        <TabPanel value={value} index={2}></TabPanel>
+        <TabPanel value={value} index={2}>
+          <ReviewAndSend
+            handleChange={handleChange}
+            extraInfo={extraInfo}
+            details={details}
+          />
+        </TabPanel>
       </div>
     </div>
   );
