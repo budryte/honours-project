4e3f53c5c420926b8f01e3a1601427642b0c3851
budryte <43143936+budryte@users.noreply.github.com>
2022-01-12 20:32:38 +0200

    navbar updated - redirecting added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,27 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
-import Button from "@mui/material/Button";
 import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
+import CloseIcon from "@mui/icons-material/Close";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useNavigate } from "react-router-dom";
+import { getAuth } from "firebase/auth";
 
 import logo from "../../images/logo.png";
 import "./navbar.scss";
 
-export default function Navbar() {
+export default function Navbar(props) {
+  //console.log("position: ", props.position);
+  let navigate = useNavigate();
+
+  const handleLogout = () => {
+    const auth = getAuth();
+    auth.signOut();
+  };
+
   const [state, setState] = React.useState({
     left: false,
   });
@@ -30,46 +41,91 @@ export default function Navbar() {
     <Box
       className="menu-drawer"
       role="presentation"
-      onClick={toggleDrawer(anchor, false)}
+      //onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
+      <div className="close-button">
+        <CloseIcon fontSize="large" onClick={toggleDrawer(anchor, false)} />
+      </div>
       <div className="image">
         <img src={logo} alt="" />
       </div>
-      <List className="menu-list">
-        <ListItem button>
+      <List>
+        <ListItem
+          button
+          style={{ "padding-left": "40px" }}
+          onClick={() => {
+            navigate("/home");
+          }}
+        >
+          <ListItemText primary="Home" />
+        </ListItem>
+
+        <ListItem button style={{ "padding-left": "40px" }}>
           <ListItemText primary="My Account" />
         </ListItem>
 
-        <ListItem button>
-          <ListItemText primary="New Request" />
-        </ListItem>
+        {props.position !== "Technician" && (
+          <ListItem
+            button
+            style={{ "padding-left": "40px" }}
+            onClick={() => {
+              navigate("/agreement");
+            }}
+          >
+            <ListItemText primary="New Request" />
+          </ListItem>
+        )}
 
-        <ListItem button>
-          <ListItemText primary="Track Request" />
-        </ListItem>
+        {props.position !== "Technician" && (
+          <ListItem
+            button
+            style={{ "padding-left": "40px" }}
+            onClick={() => {
+              navigate("/track-requests");
+            }}
+          >
+            <ListItemText primary="Track Request" />
+          </ListItem>
+        )}
 
-        <ListItem button>
-          <ListItemText primary="Pending Requests" />
-        </ListItem>
+        {props.position === "Supervisor" && (
+          <ListItem
+            button
+            style={{ "padding-left": "40px" }}
+            onClick={() => {
+              navigate("/pending-requests");
+            }}
+          >
+            <ListItemText primary="Pending Requests" />
+          </ListItem>
+        )}
 
-        <ListItem button>
-          <ListItemText primary="List of Requests" />
-        </ListItem>
+        {props.position === "Technician" && (
+          <ListItem button style={{ "padding-left": "40px" }}>
+            <ListItemText primary="List of Requests" />
+          </ListItem>
+        )}
 
-        <ListItem button>
-          <ListItemText primary="My Work" />
-        </ListItem>
+        {props.position === "Technician" && (
+          <ListItem button style={{ "padding-left": "40px" }}>
+            <ListItemText primary="My Work" />
+          </ListItem>
+        )}
 
-        <ListItem button>
+        <ListItem button style={{ "padding-left": "40px" }}>
           <ListItemText primary="My Archive" />
         </ListItem>
       </List>
 
       <Divider />
-      <List className="menu-list">
-        <ListItem button>
-          <ListItemText primary="Log Out" />
+      <List>
+        <ListItem
+          button
+          style={{ "padding-left": "40px" }}
+          onClick={handleLogout}
+        >
+          <ListItemText style={{ "font-weight": "bold" }} primary="Log Out" />
         </ListItem>
       </List>
     </Box>
@@ -79,7 +135,13 @@ export default function Navbar() {
     <div>
       {["left"].map((anchor) => (
         <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+          <MenuIcon
+            className="hamburger"
+            fontSize="large"
+            onClick={toggleDrawer(anchor, true)}
+          >
+            {anchor}
+          </MenuIcon>
           <Drawer
             anchor={anchor}
             open={state[anchor]}
