9c8d014a53413b0c31a8925e0b99abcbdc1ed58c
budryte <43143936+budryte@users.noreply.github.com>
2022-01-16 20:37:10 +0200

    saving email, firstname and lastname in the local db

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 import Dexie from "dexie";
 
 export const db = new Dexie("myDatabase");
-db.version(7).stores({
+db.version(9).stores({
   //   friends: '++id, name, age', // Primary key and indexed props
-  users: "++id, position",
+  users: "++id, position, email, firstname, lastname",
 });
