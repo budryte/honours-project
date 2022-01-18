b87a9265e7a47a96fe906c8c5b09ac93397a7bac
budryte <43143936+budryte@users.noreply.github.com>
2022-01-18 18:46:18 +0200

    list of request logic added

--- src/components/list-of-requests/ListOfRequests.jsx
+++ src/components/list-of-requests/ListOfRequests.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../navbar/Navbar";
 import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
+import {
+  collectionGroup,
+  query,
+  where,
+  getDocs,
+  getFirestore,
+} from "firebase/firestore";
+import { useLiveQuery } from "dexie-react-hooks";
+import { db as dexieDB } from "../../config/db";
 
 import "./list-of-requests.scss";
 
 export default function ListofRequests() {
   let navigate = useNavigate();
+  const users = useLiveQuery(() => dexieDB.users.toArray());
+  const [requests, setRequests] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const querySnapshot = await getDocs(
+        query(collectionGroup(getFirestore(), "requests"))
+      );
+      let reqArr = [];
+      querySnapshot.forEach((doc) => {
+        reqArr.push({ parentId: doc.ref.parent.parent.id, data: doc.data() });
+      });
+      setRequests(reqArr);
+    })();
+  }, [users]);
 
   return (
     <div>
@@ -16,26 +40,39 @@ export default function ListofRequests() {
       <div className="box">
         <div className="page-title">List of Requests</div>
         <div className="list-container">
-          {/* todo: if pending request exists, then show list with map */}
-          <List>
-            <ListItem>
-              <Button
-                onClick={() => navigate("/pick-up-request")}
-                className="list-of-request-button"
-              >
-                <div className="list-of-request-item">
-                  <div>
-                    <p>
-                      <b className="id">RTA ID </b> Date: 15/11/21
-                    </p>
-                  </div>
-                  <p>
-                    <b>Pending approval</b>
-                  </p>
-                </div>
-              </Button>
-            </ListItem>
-          </List>
+          {requests.length > 0 ? (
+            <List>
+              {requests.map((req) => (
+                <ListItem key={req.data.id}>
+                  <Button
+                    onClick={() =>
+                      navigate("/pick-up-request", {
+                        state: { data: req.data, parentId: req.parentId },
+                      })
+                    }
+                    className="list-of-request-button"
+                  >
+                    <div className="list-of-request-item">
+                      <div>
+                        <p>
+                          <b className="id">{req.data.id} </b>
+                          {"Date: "}
+                          {new Date(
+                            req.data.time.seconds * 1000
+                          ).toLocaleDateString()}
+                        </p>
+                      </div>
+                      <p>
+                        <b>{req.data.status}</b>
+                      </p>
+                    </div>
+                  </Button>
+                </ListItem>
+              ))}
+            </List>
+          ) : (
+            <p>There are no pending requests.</p>
+          )}
         </div>
       </div>
     </div>
