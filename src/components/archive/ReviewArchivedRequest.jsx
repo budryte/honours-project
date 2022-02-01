599d7443bfd0f22807f2d1682233fb4a2fdb4c5b
budryte <43143936+budryte@users.noreply.github.com>
2022-02-01 21:55:15 +0000

    removed redundant code
    improved usability

--- src/components/archive/ReviewArchivedRequest.jsx
+++ src/components/archive/ReviewArchivedRequest.jsx
@@ -1,88 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "../navbar/Navbar";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableRow from "@mui/material/TableRow";
-import { Button } from "@mui/material";
-import { useNavigate, useLocation } from "react-router-dom";
-import { doc, updateDoc, getFirestore } from "firebase/firestore";
-
-async function updateDetails(grant, account, parentId, id) {
-  const db = getFirestore();
-  console.log(parentId);
-  const requestRef = doc(db, "users", parentId, "requests", id);
-  await updateDoc(requestRef, {
-    grant: grant,
-    account: account,
-    status: "Waiting on technician",
-  });
-}
+import BackButton from "../small-components/BackButon";
+import SupervisorDetails from "../small-components/SupervisorDetails";
+import MainRequestDetails from "../small-components/MainRequestDetails";
+import TechnicianDetails from "../small-components/TechnicianDetails";
+import { Grid } from "@mui/material";
 
 export default function ReviewArchivedRequest() {
-  let navigate = useNavigate();
-  const { state } = useLocation();
-  const { time, estimatedTime, id, firstname, lastname, email, ...details } =
-    state.data;
-
   return (
     <div>
       <Navbar />
       <div className="box">
         <div className="page-title">Archived Request</div>
         <div className="white-container">
-          <h2>{id}</h2>
-          <h4>
-            Requested by {firstname} {lastname}
-            <br></br>
-            {email}
-          </h4>
-          <TableContainer>
-            <Table sx={{ minWidth: 450 }} aria-label="simple table">
-              <TableBody>
-                {Object.keys(details ?? {})?.map((key) => (
-                  <TableRow key={key}>
-                    <TableCell component="th" scope="row">
-                      {(key.charAt(0).toUpperCase() + key.slice(1))
-                        .match(/([A-Z]?[^A-Z]*)/g)
-                        .slice(0, -1)
-                        .join(" ")}
-                    </TableCell>
-                    <TableCell>{details[key]}</TableCell>
-                  </TableRow>
-                ))}
-                <TableRow key={"time"}>
-                  <TableCell component="th" scope="row">
-                    Date Submitted
-                  </TableCell>
-                  <TableCell>
-                    {new Date(time.seconds * 1000).toGMTString()}
-                  </TableCell>
-                </TableRow>
-                <TableRow key={"estimatedTime"}>
-                  <TableCell component="th" scope="row">
-                    Date finished
-                  </TableCell>
-                  <TableCell>
-                    {new Date(estimatedTime.seconds * 1000).toLocaleString()}
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <div className="buttons">
-            <div className="request-form-button">
-              <Button
-                variant="contained"
-                onClick={() => {
-                  navigate("/archive");
-                }}
-              >
-                Back
-              </Button>
-            </div>
-          </div>
+          <Grid container spacing={2}>
+            <Grid item xs={1}>
+              <div className="back-button">
+                <BackButton pageTitle={"/archive"} />
+              </div>
+            </Grid>
+            <Grid item xs={11}>
+              <MainRequestDetails />
+              <SupervisorDetails />
+              <TechnicianDetails />
+            </Grid>
+          </Grid>
         </div>
       </div>
     </div>
--- src/components/pending-requests/ReviewPendingRequest.jsx
+++ src/components/pending-requests/ReviewPendingRequest.jsx
@@ -1,15 +1,10 @@
 import React, { useState } from "react";
 import Navbar from "../navbar/Navbar";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableRow from "@mui/material/TableRow";
-import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
-import Modal from "@mui/material/Modal";
+import BackButton from "../small-components/BackButon";
+import SupervisorDetails from "../small-components/SupervisorDetails";
+import MainRequestDetails from "../small-components/MainRequestDetails";
+import TechnicianDetails from "../small-components/TechnicianDetails";
+import { Grid, Button, Box, Typography, TextField, Modal } from "@mui/material";
 import { useNavigate, useLocation } from "react-router-dom";
 import { doc, updateDoc, getFirestore } from "firebase/firestore";
 
@@ -39,7 +34,7 @@ export default function ReviewPendingRequest() {
 
   let navigate = useNavigate();
   const { state } = useLocation();
-  const { time, id, firstname, lastname, email, ...details } = state.data;
+  const { id } = state.data;
   const { parentId } = state;
 
   return (
@@ -48,154 +43,128 @@ export default function ReviewPendingRequest() {
       <div className="box">
         <div className="page-title">Review Request</div>
         <div className="white-container">
-          <h2>{id}</h2>
-          <h4>
-            Requested by {firstname} {lastname}
-            <br></br>
-            {email}
-          </h4>
-          <TableContainer>
-            <Table sx={{ minWidth: 450 }} aria-label="simple table">
-              <TableBody>
-                {Object.keys(details ?? {})?.map((key) => (
-                  <TableRow key={key}>
-                    <TableCell component="th" scope="row">
-                      {(key.charAt(0).toUpperCase() + key.slice(1))
-                        .match(/([A-Z]?[^A-Z]*)/g)
-                        .slice(0, -1)
-                        .join(" ")}
-                    </TableCell>
-                    <TableCell>{details[key]}</TableCell>
-                  </TableRow>
-                ))}
-                <TableRow key={"time"}>
-                  <TableCell component="th" scope="row">
-                    Date Submitted
-                  </TableCell>
-                  <TableCell>
-                    {new Date(time.seconds * 1000).toGMTString()}
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <div className="buttons">
-            <div className="request-form-button">
-              <Button
-                variant="contained"
-                onClick={() => {
-                  navigate("/pending-requests");
+          <Grid container spacing={2}>
+            <Grid item xs={1}>
+              <div className="back-button">
+                <BackButton pageTitle={"/pending-requests"} />
+              </div>
+            </Grid>
+            <Grid item xs={11}>
+              <MainRequestDetails />
+              <SupervisorDetails />
+              <TechnicianDetails />
+              <div className="buttons">
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    handleOpen();
+                  }}
+                >
+                  Approve
+                </Button>
+              </div>
+            </Grid>
+          </Grid>
+          {/* Sign request modal */}
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box className="modal-style">
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Approve Request
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Enter account to be charged:
+              </Typography>
+              <TextField
+                className="form-group"
+                id="outlined-basic"
+                label="Account to be charged"
+                variant="outlined"
+                size="small"
+                required
+                value={account}
+                onChange={(e) => {
+                  setAccount(e.target.value);
                 }}
-              >
-                Back
-              </Button>
-            </div>
-            <Button
-              variant="contained"
-              onClick={() => {
-                handleOpen();
-              }}
-            >
-              Sign
-            </Button>
-            <Modal
-              open={open}
-              onClose={handleClose}
-              aria-labelledby="modal-modal-title"
-              aria-describedby="modal-modal-description"
-            >
-              <Box className="modal-style">
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                  Approve Request
-                </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                  Enter account to be charged:
-                </Typography>
-                <TextField
-                  className="form-group"
-                  id="outlined-basic"
-                  label="Account to be charged"
-                  variant="outlined"
-                  size="small"
-                  required
-                  value={account}
-                  onChange={(e) => {
-                    setAccount(e.target.value);
-                  }}
-                />
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                  Enter student grant:
-                </Typography>
-                <TextField
-                  className="form-group"
-                  id="outlined-basic"
-                  label="Grant"
-                  variant="outlined"
-                  size="small"
-                  required
-                  value={grant}
-                  onChange={(e) => {
-                    setGrant(e.target.value);
-                  }}
-                />
-                <div className="modal-buttons">
-                  <div className="request-form-button">
-                    <Button
-                      variant="outlined"
-                      color="error"
-                      onClick={() => {
-                        handleClose();
-                      }}
-                    >
-                      Cancel
-                    </Button>
-                  </div>
+              />
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Enter student grant:
+              </Typography>
+              <TextField
+                className="form-group"
+                id="outlined-basic"
+                label="Grant"
+                variant="outlined"
+                size="small"
+                required
+                value={grant}
+                onChange={(e) => {
+                  setGrant(e.target.value);
+                }}
+              />
+              <div className="modal-buttons">
+                <div className="request-form-button">
                   <Button
                     variant="outlined"
-                    color="success"
-                    disabled={grant === "" || account === ""}
+                    color="error"
                     onClick={() => {
-                      addFields(grant, account, parentId, id);
                       handleClose();
-                      handleConfirmationOpen();
                     }}
                   >
-                    Sign
+                    Cancel
                   </Button>
                 </div>
-              </Box>
-            </Modal>
-            <Modal
-              open={confirmationOpen}
-              onClose={() => {
-                navigate("/home");
-              }}
-              aria-labelledby="modal-modal-title"
-              aria-describedby="modal-modal-description"
-            >
-              <Box className="modal-style">
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                  Confirmation
-                </Typography>
-                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                  Your request has been approved successfully. <br></br>You can
-                  now track this request.
-                </Typography>
-                <div className="modal-buttons">
-                  <div className="request-form-button">
-                    <Button
-                      variant="outlined"
-                      onClick={() => {
-                        navigate("/track-requests");
-                      }}
-                    >
-                      Track Request
-                    </Button>
-                  </div>
+                <Button
+                  variant="outlined"
+                  color="success"
+                  disabled={grant === "" || account === ""}
+                  onClick={() => {
+                    addFields(grant, account, parentId, id);
+                    handleClose();
+                    handleConfirmationOpen();
+                  }}
+                >
+                  Save
+                </Button>
+              </div>
+            </Box>
+          </Modal>
+
+          {/* Confirmation modal */}
+          <Modal
+            open={confirmationOpen}
+            onClose={() => {
+              navigate("/home");
+            }}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box className="modal-style">
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Confirmation
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Your request has been approved successfully. <br></br>You can
+                now track this request.
+              </Typography>
+              <div className="modal-buttons">
+                <div className="request-form-button">
+                  <Button
+                    variant="outlined"
+                    onClick={() => {
+                      navigate("/track-requests");
+                    }}
+                  >
+                    Track Request
+                  </Button>
                 </div>
-              </Box>
-            </Modal>
-          </div>
+              </div>
+            </Box>
+          </Modal>
         </div>
       </div>
     </div>
